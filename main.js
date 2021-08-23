263c5d308300a06f75612c5e966f65c814175571
AlissonAzevedo <alissonazevedo13.1@gmail.com>
2021-08-23 12:49:05 -0300

    update 0.5

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+    const cancelModal = () => document.getElementById('modal')
+    .classList.remove('active')
+
 document.getElementById('cadastrarReserva')
     .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
+
+document.getElementById('cancelModal')
+    .addEventListener('click', cancelModal)
