19f11fdff618a04fc6b82a8406335d96cc6b72bf
AlissonAzevedo <alissonazevedo13.1@gmail.com>
2021-09-02 18:55:55 -0300

    update 0.1

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
-    const cancelModal = () => document.getElementById('modal')
+const cancelModal = () => document.getElementById('modal')
     .classList.remove('active')
 
 document.getElementById('cadastrarReserva')
@@ -17,3 +17,67 @@ document.getElementById('modalClose')
 
 document.getElementById('cancelModal')
     .addEventListener('click', cancelModal)
+
+
+const Agendamento ={
+    add(agendamento){
+        Agendamento.all.push(agendamento)
+        App.reload()
+    },
+    remove(index){
+        Agendamento.all.splice(index, 1)
+        App.reload()
+    },
+    
+}
+    
+
+
+const Utils = {
+    formatdate(date){
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+    formathr(time){
+        //const 
+    }
+}
+
+const Form = {
+    name:document.querySelector('input#name'),
+    email:document.querySelector('input#email'),
+    value:document.querySelector('input#value'),
+    nameClass:document.querySelector('input#nameClass'),
+    timeOpen:document.querySelector('input#time-open'),
+    timeExit:document.querySelector('input#timeExit'),
+    date:document.querySelector('input#date'),
+
+    getValues(){
+        return{
+            name: Form.name.value,
+            email: Form.email.value,
+            value: Form.value.value,
+            nameClass: Form.nameClass.value,
+            timeOpen: Form.timeOpen.value,
+            timeExit: Form.timeExit.value,
+            date: Form.date.value,
+        }
+    },
+
+    validateFields(){
+        const {name, email, value, nameClass, timeOpen, timeExit, date} = getValues()
+        if(name.trim() === "" || email.trim() === "" || value.trim() === "" || nameClass.trim() === "" || timeOpen.trim() === "" || timeExit.trim() === "" || date.trim() === ""){
+            throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+
+    submit(event){
+        event.preventDefault()
+        try{
+            //verficar se todas as informações foram preenchidas
+            Form.validateFields()
+        }catch(error){
+            alert(error.message)
+        }
+    }
+}
